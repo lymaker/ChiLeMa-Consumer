0149d3f2b556cafb53d5c8ec883c83f37d4490d0
LiuYun <1265894132@qq.com>
2023-01-08 17:41:37 +0800

    修改 LayoutDefault.vue TabBar 实现

--- src/layout/default/LayoutDefault.vue
+++ src/layout/default/LayoutDefault.vue
@@ -4,61 +4,48 @@
       <router-view />
     </main>
     <footer>
-      <nav>
-        <div
+      <van-tabbar v-model="active">
+        <van-tabbar-item
           v-for="(item, index) in nav"
           :key="index"
-          @click="clickNavItem(index)"
+          :to="item.path"
+          :icon="item.icon"
         >
-          <van-icon
-            :name="active === index ? item.activeIcon : item.defaultIcon"
-            size="22"
-          />
-          <h5>{{ item.title }}</h5>
-        </div>
-      </nav>
+          {{ item.title }}
+        </van-tabbar-item>
+      </van-tabbar>
     </footer>
   </div>
 </template>
 
 <script setup>
-import {useRoute, useRouter} from 'vue-router';
+import {useRoute} from 'vue-router';
 
-const router = useRouter();
 const route = useRoute();
 
 const nav = [
   {
     path: '/home',
     title: '首页',
-    defaultIcon: 'https://img.icons8.com/material-outlined/48/null/home--v2.png',
-    activeIcon: 'https://img.icons8.com/material/48/null/home--v5.png'
+    icon: 'wap-home-o',
   },
   {
     path: '/cart',
     title: '购物车',
-    defaultIcon: 'https://img.icons8.com/material-outlined/48/null/shopping-cart--v1.png',
-    activeIcon: 'https://img.icons8.com/material/48/null/shopping-cart--v1.png'
+    icon: 'shopping-cart-o',
   },
   {
     path: '/order',
     title: '订单',
-    defaultIcon: 'https://img.icons8.com/material-outlined/48/null/bill--v1.png',
-    activeIcon: 'https://img.icons8.com/material/48/null/bill--v1.png'
+    icon: 'orders-o',
   },
   {
     path: '/my',
     title: '我的',
-    defaultIcon: 'https://img.icons8.com/material-outlined/48/1A1A1A/gender-neutral-user.png',
-    activeIcon: 'https://img.icons8.com/material/48/1A1A1A/gender-neutral-user.png'
+    icon: 'user-o',
   }
 ];
 let active = $ref(nav.findIndex(e => e.path === route.path));
-
-function clickNavItem(index) {
-  active = index;
-  router.push(nav[index].path);
-}
 </script>
 
 <style lang="less" scoped>
